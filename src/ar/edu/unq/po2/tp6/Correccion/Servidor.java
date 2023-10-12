819558c8003e2b1f39448de0319134172759265a
publico <publico@infor37b23.alu.unq.edu.ar>
2023-10-12 15:40:41 -0300

    solo una interface para servidor

--- src/ar/edu/unq/po2/tp6/Correccion/Servidor.java
+++ src/ar/edu/unq/po2/tp6/Correccion/Servidor.java
@@ -1,7 +1,12 @@
 package ar.edu.unq.po2.tp6.Correccion;
 
+import java.util.List;
+
 public interface Servidor {
 	
 	public void conectar(String nombreUsuario, String pass);
+	public void enviar(Correo correo);
+
+	public List<Correo> recibir();
 
 }
